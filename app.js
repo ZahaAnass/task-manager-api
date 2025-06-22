fc30c465134c0f2101bef5205a89d0cdda62040b
ZahaAnass <zahaanass277@gmail.com>
2025-06-22 16:26:21 +0100

    add the env file for connection to db

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -23,6 +24,15 @@ app.use("/api/v1/tasks", tasks)
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-});
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is running on port: ${port}...`)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
