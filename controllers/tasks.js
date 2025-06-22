af74799eba0e0f7d56b1427448c95ba2d9d2e749
ZahaAnass <zahaanass277@gmail.com>
2025-06-22 20:00:10 +0100

    finish getAllTasks task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require("../models/tasks")
 
 const getAllTasks = (req, res) => {
-    res.send("all tasks")
+    try {
+        const tasks = Task.find()
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
+    }
 }
 
 const createTask = async (req, res) => {
