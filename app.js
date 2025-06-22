1a3498c929e64262a237c5a391fda0886b1024d5
ZahaAnass <zahaanass277@gmail.com>
2025-06-22 16:16:41 +0100

    update the app and require the db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 // middleware
 app.use(express.json());
