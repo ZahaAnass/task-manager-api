8009deb3322097eb937a03d8b106e23ef533821d
ZahaAnass <zahaanass277@gmail.com>
2025-06-24 19:22:48 +0100

    use the middlewares and also the port env

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found")
+const errorHandler = require("./middleware/error-handler")
 require("dotenv").config();
 
 // middleware
@@ -14,7 +15,7 @@ require("dotenv").config();
 // routes
 app.use("/api/v1/tasks", tasks)
 app.use(notFound);
-
+app.use(errorHandler);
 
 // app.get("/api/v1/tasks")             - get all the tasks
 // app.post("/api/v1/tasks")            - create a new task
@@ -22,7 +23,7 @@ app.use(notFound);
 // app.patch("/api/v1/tasks/:id")       - update task 
 // app.delete("/api/v1/tasks/:id")      - delete task
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
