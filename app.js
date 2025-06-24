79c64e16eff6d03f3eb5b1e27d576b8771085b4f
ZahaAnass <zahaanass277@gmail.com>
2025-06-24 16:49:22 +0100

    use middleware

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found")
 require("dotenv").config();
 
 // middleware
-app.use(express.json());
-
+    // static files
+    app.use(express.static("./public"));
+    // parse json
+    app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-    res.send("Task Manager APP");
-})
-
 app.use("/api/v1/tasks", tasks)
+app.use(notFound);
 
 
 // app.get("/api/v1/tasks")             - get all the tasks
