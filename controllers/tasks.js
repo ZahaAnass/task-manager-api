0d2e6ef2ef8deac761551b0ca77927ab9da43968
ZahaAnass <zahaanass277@gmail.com>
2025-06-24 15:09:19 +0100

    update the the update api

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -25,20 +25,7 @@ const getTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({ msg: `No task with id : ${taksID}` })
         }
-        res.json({ task })
-    } catch (error) {
-        res.status(500).json({ msg: error.message })
-    }
-}
-
-const updateTask = async (req, res) => {
-    try {
-        const { id:taksID } = req.params;
-        const task = await Task.findOne({ _id: taksID })
-        if (!task) {
-            return res.status(404).json({ msg: `No task with id : ${taksID}` })
-        }
-        res.json({ task })
+        res.status(200).json({ task })
     } catch (error) {
         res.status(500).json({ msg: error.message })
     }
@@ -57,6 +44,21 @@ const deleteTask = async (req, res) => {
     }
 }
 
+const updateTask = async (req, res) => {
+    try {
+        const { id:taksID } = req.params;
+        const task = await Task.findOneAndUpdate({ _id: taksID }, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id : ${taksID}` })
+        }
+        res.status(200).json({ id:taksID, data:req.body })
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
+    }
+}
 
 module.exports = {
     getAllTasks,
